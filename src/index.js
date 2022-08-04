576260fab837365a3e5427a4fdd47d5bd28a8789
dentremontcatwit <dentremontc@wit.edu>
2022-08-03 22:23:30 -0400

    Add google signin

--- src/index.js
+++ src/index.js
@@ -271,11 +271,10 @@ function signInGoogle() {
   signInWithPopup(auth, provider)
     .then((result) => {
       const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
       const user = result.user;
       var settings = {};
       settings["userSettings"] = ["true", "2 Weeks", "Cards"]; //Default settings
-      setDoc(doc(db, "users", cred.user.uid), settings);
+      setDoc(doc(db, "users", user.uid), settings);
       location.reload();
     })
     .catch((e) => {
